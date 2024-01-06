e25e3531db6a1b440d80ee010e5f1d3912674dfb
JOAQUIN MORALES NIETO <jmn034@inlumine.ual.es>
2024-01-06 17:56:47 +0100

    Player Detail View

---
+++ ProyectoLPS/Views/PlayerDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerDetailsView.swift
+//  ProyectoLPS
+//
+//  Created by Aula03 on 6/1/24.
+//
+
+import SwiftUI
+
+struct PlayerDetailsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PlayerDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerDetailsView()
+    }
+}
