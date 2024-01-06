0e7182c84d2d86e7022caad4fb50955b0a6964ca
JOAQUIN MORALES NIETO <jmn034@inlumine.ual.es>
2024-01-06 17:42:19 +0100

    FetchModel

--- FetchModel/FetchModel.swift
+++ FetchModel/FetchModel.swift
@@ -91,8 +91,11 @@ struct ShotPower: Decodable {
     let value: Int
 }
 
-
-
+struct Equipo: Identifiable, Decodable {
+    let id = UUID()
+    let name: String
+    let image: String
+}
 
 struct Response: Decodable {
     let next: Page
