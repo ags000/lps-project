bae0420fe8b2e0f8d8572b4be0dc622ebe537ff8
Cristian Gil García <cgg734@inlumine.ual.es>
2024-01-07 19:08:54 +0100

    Vista haaland y alexia

---
+++ ProyectoLPS/Views/VistaAlexia.swift
@@ -0,0 +1,20 @@
+//
+//  VistaAlexia.swift
+//  ProyectoLPS
+//
+//  Created by Aula03 on 7/1/24.
+//
+
+import SwiftUI
+
+struct VistaAlexia: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VistaAlexia_Previews: PreviewProvider {
+    static var previews: some View {
+        VistaAlexia()
+    }
+}
---
+++ ProyectoLPS/Views/VistaHaaland.swift
@@ -0,0 +1,20 @@
+//
+//  VistaHaaland.swift
+//  ProyectoLPS
+//
+//  Created by Aula03 on 7/1/24.
+//
+
+import SwiftUI
+
+struct VistaHaaland: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VistaHaaland_Previews: PreviewProvider {
+    static var previews: some View {
+        VistaHaaland()
+    }
+}
